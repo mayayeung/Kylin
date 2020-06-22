aa8664c641a954026228d762b41c8a8a20ffac5a
dingjinzhu <jzding1314@126.com>
2020-06-22 17:24:28 +0800

    面包屑功能测试

--- CMPT_player/src/main/java/com/martin/cmpt/player/PlayerActivity.java
+++ CMPT_player/src/main/java/com/martin/cmpt/player/PlayerActivity.java
@@ -3,8 +3,11 @@ package com.martin.cmpt.player;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import com.martin.core.utils.CrumbView;
 
 public class PlayerActivity extends AppCompatActivity {
 
@@ -12,7 +15,16 @@ public class PlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.core_activity_main);
-        ((TextView) findViewById(R.id.core_tv)).setText("Player component page!");
+
+        CrumbView crumbView = (CrumbView) findViewById(R.id.crumb_view);
+        crumbView.setActivity(this);
+
+        int firstLevel = 1;
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.setBreadCrumbTitle(getString(R.string.crumb_title, firstLevel));
+        ft.replace(R.id.frag_container, MyFragment.getInstance(firstLevel));
+        ft.addToBackStack(null);
+        ft.commitAllowingStateLoss();
     }
 
     public static void launchSelf(Context context) {
