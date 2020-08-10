4939621e4f8b92315ad71bd6c3d612ff7ca1a5f9
dingjinzhu <jzding1314@126.com>
2020-08-10 10:29:25 +0800

    处理gradle中文乱码问题

--- CMPT_player/src/main/java/com/martin/cmpt/player/PlayerActivity.java
+++ CMPT_player/src/main/java/com/martin/cmpt/player/PlayerActivity.java
@@ -3,14 +3,23 @@ package com.martin.cmpt.player;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewParent;
+import android.widget.LinearLayout;
 
-public class PlayerActivity extends AppCompatActivity {
+public class PlayerActivity extends FragmentActivity {
+    private LinearLayout mainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.player_activity_main);
+        mainLayout = findViewById(R.id.main_layout);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View button = inflater.inflate(R.layout.button_layout, mainLayout,true);
     }
 
     public static void launchSelf(Context context) {
