51953e1d23f76b68f068e48033aacaa53d5ef2a5
dingjinzhu <jzding1314@126.com>
2022-01-17 18:05:30 +0800

    canvas相关操作测试

--- CMPT_demo/src/main/java/com/martin/cmpt/demo/utils/SimpleDoodleView.java
+++ CMPT_demo/src/main/java/com/martin/cmpt/demo/utils/SimpleDoodleView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -35,6 +36,52 @@ public class SimpleDoodleView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawPath(path,paint);
+/*
+
+        paint.setStrokeWidth(20);
+        canvas.drawPoint(200,200,paint);
+
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(5);
+        canvas.drawCircle(200,200,100,paint);
+
+        canvas.translate(200, 200);
+        paint.setColor(Color.BLACK);
+        canvas.drawCircle(200,0,100,paint);
+*/
+
+
+        canvas.translate(400, 800);
+        paint.setStrokeWidth(20);
+        paint.setColor(Color.RED);
+        canvas.drawPoint(0, 0, paint);
+
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(5);
+        canvas.drawLine(-400, 0, getWidth() - 400, 0, paint);
+        canvas.drawLine(0, -800, 0, getHeight() - 800, paint);
+
+        paint.setColor(Color.BLUE);
+        Rect rt = new Rect(0, -400, 400, 0);
+        canvas.drawRect(rt, paint);
+
+        canvas.save();
+        canvas.scale(1.3f, 1.3f);
+        paint.setColor(Color.GREEN);
+        canvas.drawRect(rt, paint);
+        canvas.restore();
+        canvas.drawLine(0, 0, 400, -400, paint);
+
+        canvas.save();
+//        canvas.scale(1f, 1f, 200, 0);
+        canvas.translate(200, 0);
+        canvas.scale(0.5f, 0.5f);
+        paint.setColor(Color.BLUE);
+        paint.setStrokeWidth(50);
+        canvas.drawPoint(0, 0, paint);
+//        paint.setColor(Color.MAGENTA);
+//        paint.setStrokeWidth(5);
+//        canvas.drawRect(rt, paint);
     }
 
     @Override
